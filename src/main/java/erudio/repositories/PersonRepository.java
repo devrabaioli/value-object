cf0565dff7506a07aae4b56db84438f181dd36b7
devrabaioli <devrabaioli@gmail.com>
2023-03-24 11:42:17 +0000

    Value Object

---
+++ src/main/java/erudio/repositories/PersonRepository.java
@@ -0,0 +1,12 @@
+package erudio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import erudio.model.Person;
+
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
